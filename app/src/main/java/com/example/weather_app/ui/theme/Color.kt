fb3cb1aaff4427658b70a2f26b0c7bce12cc3146
Amina <dosanamina.007@gmail.com>
2026-02-01 22:47:23 +0500

    Main Activity

---
+++ app/src/main/java/com/example/weather_app/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.weather_app.ui.theme
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+val backgroundBrush = Brush.verticalGradient(
+    colors = listOf(
+        Color(0xFF0F2027),
+        Color(0xFF203A43),
+        Color(0xFF2C5364)
+    )
+)
---
+++ app/src/main/java/com/example/weather_app/ui/theme/Theme.kt
@@ -0,0 +1,28 @@
+package com.example.weather_app.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Color.White,
+    secondary = Color.White,
+    background = Color.Black,
+    surface = Color.Black,
+    onPrimary = Color.Black,
+    onSecondary = Color.Black,
+    onBackground = Color.White,
+    onSurface = Color.White
+)
+
+@Composable
+fun WeatherAppTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = DarkColorScheme,
+        typography = Typography,
+        content = content
+    )
+}
